8031d876c651c920385dfe4f3a9abb120c22884a
yazen firas <psyazen3@gmail.com>
2020-06-05 19:37:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName,userEmail){
+  return fetch("http://localhost:3000/users",{
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: `${userName}`,
+    email : `${userEmail}`
+  })
+}).then(response=>{
+  return response.json()
+}).then(Object=>{
+
+  document.body.innerHtml=Object["id"]
+}).catch(err=>{
+  document.body.innerHtml=err.message
+})
+
+}
