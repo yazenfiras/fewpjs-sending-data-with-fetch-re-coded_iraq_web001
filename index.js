925a9299c8c405d15487b86ff69753079675c243
yazen firas <psyazen3@gmail.com>
2020-06-08 09:50:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your code here
-function submitData(userName,userEmail){
+/* function submitData(userName,userEmail){
   return fetch("http://localhost:3000/users",{
   method: "POST",
   headers: {
@@ -13,6 +13,7 @@ function submitData(userName,userEmail){
 }).then(response=>{
   return response.json()
 }).then(Object=>{
+  console.log(Object)
 
   document.body.innerHtml=Object["id"]
 }).catch(err=>{
@@ -20,3 +21,41 @@ function submitData(userName,userEmail){
 })
 
 }
+let name = "jim"
+      let email = "jim@jim.com"
+submitData(name,email)
+*/
+
+function submitData(name,email)
+{
+  let info = {
+        name:name,
+        email:email
+      }
+  let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(info)
+};
+  return fetch("http://localhost:3000/users",configObj)
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let div = document.createElement('div');
+        div.innerHTML = object.id;
+        document.querySelector('body').append(div);
+  })
+  .catch(function(error) {
+          let div = document.createElement('div');
+          div.innerHTML = error.message;
+          document.querySelector('body').append(div);
+        });
+      };
+      let name = "jim"
+      let email="jim@jim.com"
+      submitData(name,email)
+  // Add your code here
